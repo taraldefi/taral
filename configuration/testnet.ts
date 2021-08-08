43b4255650dbb6cf5bc9f2933c772f58f54f532f
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-08 16:52:10 +0300

    feat(configuration): add new configuration wallets

--- configuration/testnet.ts
+++ configuration/testnet.ts
@@ -1,57 +1,29 @@
 import { pubKeyfromPrivKey, publicKeyToString } from "@stacks/transactions";
 
 // from Stacks config.toml file
-// export const ADDR1 = "ST1HJ4TYWQV3MCSP2T751GDN39PTENCX72HPQYDCM";
-// export const ADDR2 = "STRVCQ501XZVWWWBWQTC3BCWKYG8WWER1BSCD9S8";
-// export const ADDR3 = "ST1MFSFY91ZZFCVQFRPHYH2HS258D962RPK3M8CW5";
-// export const ADDR4 = "ST1Z6E1B35N3A8C974PPCMQPVJ10SCEJA68PMPQ95";
-// export const testnetKeys: { secretKey: string; stacksAddress: string }[] = [
-//   {
-//     secretKey:
-//       "d8b6639a9dc544cb52accaa701b45c9a3320519101fae0cb90c8066ec7e1d6c601",
-//     stacksAddress: ADDR1,
-//   },
-//   {
-//     secretKey:
-//       "95ef516f20d266f6972ccb72382e74a0de7e872dcef764924d6da0c688723a0301",
-//     stacksAddress: ADDR2,
-//   },
-//   {
-//     secretKey:
-//       "97a0af6784770897281b17e628a794b31f6acad422624c166df02ad4b7fd5c5f01",
-//     stacksAddress: ADDR3,
-//   },
-//   {
-//     secretKey:
-//       "8d61a7c285049ec0a98a02c425f9717c6c9cd103fa4e7bf0f6a30559eedf6dab01",
-//     stacksAddress: ADDR4,
-//   },
-// ];
-
-// from Stacks config.toml file
-export const ADDR1 = "ST2ZRX0K27GW0SP3GJCEMHD95TQGJMKB7G9Y0X1MH";
-export const ADDR2 = "ST26FVX16539KKXZKJN098Q08HRX3XBAP541MFS0P";
-export const ADDR3 = "ST3CECAKJ4BH08JYY7W53MC81BYDT4YDA5M7S5F53";
-export const ADDR4 = "ST31HHVBKYCYQQJ5AQ25ZHA6W2A548ZADDQ6S16GP";
+export const ADDR1 = "ST228ADYKA0VKDSZXCA4E13MB38SG3EZJTZY9EPJR";
+export const ADDR2 = "ST31Q2S6CXS4WTQS92CR1GCGD0W9PRHSFZZH8XXRH";
+export const ADDR3 = "ST22ZZ99DRQQZ3HATGYRKCYKVZY78Z1ZAWBA7QE2P";
+export const ADDR4 = "ST2604KQ6A2TD9EP1FMAJ6BESC22H0E557KQHB7CV";
 export const testnetKeys: { secretKey: string; stacksAddress: string }[] = [
   {
     secretKey:
-      "b8d99fd45da58038d630d9855d3ca2466e8e0f89d3894c4724f0efc9ff4b51f001",
+      "2b14b8c3d746d2d5fd8fdfdc55200dacb46f276dceff2f90f8342eed35e92a1501",
     stacksAddress: ADDR1,
   },
   {
     secretKey:
-      "3a4e84abb8abe0c1ba37cef4b604e73c82b1fe8d99015cb36b029a65099d373601",
+      "793ec7a986c3510d582c3ad3522c6ebd258861508aedbeb5b6de0182295eab6d01",
     stacksAddress: ADDR2,
   },
   {
     secretKey:
-      "052cc5b8f25b1e44a65329244066f76c8057accd5316c889f476d0ea0329632c01",
+      "4db14c26f229e1c28f043da8095872dbacaec0b9c6d65808952e2ca2470abb7101",
     stacksAddress: ADDR3,
   },
   {
     secretKey:
-      "9aef533e754663a453984b69d36f109be817e9940519cc84979419e2be00864801",
+      "020ad734559ecca79065deda2b8a2c9ada4b3d078407d098ccbfb46fb4b441c401",
     stacksAddress: ADDR4,
   },
 ];
