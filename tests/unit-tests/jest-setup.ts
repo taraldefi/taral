7e7195ab7d957b2fbc89d0fb737903737dd2b883
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-11 19:14:14 +0300

    style(tests): remove console logs

--- tests/unit-tests/jest-setup.ts
+++ tests/unit-tests/jest-setup.ts
@@ -6,9 +6,7 @@ import { TaralCoinContract } from "../../src";
 export let talToken: TaralCoinContract;
 
 beforeAll(async () => {
-  console.log("Running deploy beforeall");
   jest.setTimeout(3000000);
   const deployed = await TestProvider.fromContracts(contracts);
   talToken = deployed.taralCoin.contract;
-  console.log("deployedddd");
 });
