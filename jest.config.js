79c182242400234f8a7bc2fca0ce0ad42604e1a9
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-11 19:09:40 +0300

    chore(jest): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
+const { resolve } = require("path");
+
+const root = resolve(__dirname);
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["<rootDir>/tests/unit-tests/**/*.test.ts"],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/src/taral/**/*.ts",
+    "!<rootDir>/tests/unit-tests/**/*.test.ts",
+  ],
+  coverageReporters: ["text-summary", "lcov"],
+  coverageDirectory: "coverage",
   moduleNameMapper: {
     "^@common/(.*)$": "<rootDir>/src/common/$1",
     "^@components/(.*)$": "<rootDir>/src/components/$1",
