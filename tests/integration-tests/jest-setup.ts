2fa0773096e95d9e4d5382e9cd1a5af3b6f5dc03
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-15 12:46:04 +0300

    feat(tests): accept clarinet configuration

--- tests/integration-tests/jest-setup.ts
+++ tests/integration-tests/jest-setup.ts
@@ -1,24 +1,26 @@
 import { StacksNetwork } from "@stacks/network";
 import { StacksNetworkConfiguration } from "../../configuration/stacks-network";
-import { ADDR1, testnetKeyMap } from "../../configuration/testnet";
+import { ClarinetAccounts, getClarinetAccounts } from "../../shared/configuration";
 import { Logger } from "../../shared/logger";
 import { ApiProvider } from "../../shared/providers";
-import { TestProvider } from "../../shared/providers/test-provider";
 import { contracts } from "../../src";
-
 import { TaralCoinContract } from "../../src";
 
-const TOKEN_OWNER = testnetKeyMap[ADDR1];
-let taralCoin: TaralCoinContract;
-const network: StacksNetwork = new StacksNetworkConfiguration();
-
+export const network: StacksNetwork = new StacksNetworkConfiguration();
 export let talToken: TaralCoinContract;
+export let clarinetAccounts: ClarinetAccounts;
 
 beforeAll(async () => {
   jest.setTimeout(3000000);
+
+  const cwd = process.cwd();
+  clarinetAccounts = await getClarinetAccounts(cwd);
+
+  var deployer = clarinetAccounts.deployer;
+
   const deployed = await ApiProvider.fromContracts(contracts, network, {
-    secretKey: TOKEN_OWNER.secretKey,
-    stacksAddress: TOKEN_OWNER.address,
+    secretKey: deployer.privateKey,
+    stacksAddress: deployer.address,
   });
 
   Logger.debug("Deployed contracts to testnet");
--- tests/unit-tests/jest-setup.ts
+++ tests/unit-tests/jest-setup.ts
@@ -1,12 +1,17 @@
+import { ClarinetAccounts, getClarinetAccounts } from "../../shared/configuration";
 import { TestProvider } from "../../shared/providers/test-provider";
 import { contracts } from "../../src";
 
 import { TaralCoinContract } from "../../src";
 
 export let talToken: TaralCoinContract;
+export let clarinetAccounts: ClarinetAccounts;
 
 beforeAll(async () => {
   jest.setTimeout(3000000);
+
+  const cwd = process.cwd();
+  clarinetAccounts = await getClarinetAccounts(cwd);
   const deployed = await TestProvider.fromContracts(contracts);
   talToken = deployed.taralCoin.contract;
 });
