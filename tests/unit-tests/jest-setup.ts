0ae69a19ff68926ec6465ea8738ca3604fefd20c
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-16 11:45:07 +0300

    feat(unit-tests): deploy test utils also

--- tests/unit-tests/jest-setup.ts
+++ tests/unit-tests/jest-setup.ts
@@ -4,11 +4,13 @@ import { TestProvider } from "../../shared/providers/test-provider";
 import { contracts as coreContracts } from "../../generated/external/core";
 import { contracts as arkadikoContracts } from "../../generated/external/arkadiko";
 import { contracts as taralContracts } from "../../generated/taral";
+import { contracts as testUtilsContracts, TestUtilsContract } from "../../generated/test-utils";
 
 import { TaralCoinContract } from "../../generated/taral";
 import { getDefaultClarityBin } from "../../shared/adapter";
 
 export let talToken: TaralCoinContract;
+export let testToken: TestUtilsContract;
 export let clarinetAccounts: ClarinetAccounts;
 
 beforeAll(async () => {
@@ -17,6 +19,9 @@ beforeAll(async () => {
   const cwd = process.cwd();
   clarinetAccounts = await getClarinetAccounts(cwd);
   const clarityBin = await getDefaultClarityBin(clarinetAccounts);
+
+  var deployedTestUtils = await TestProvider.fromContracts(testUtilsContracts, clarityBin);
+  testToken = deployedTestUtils.testUtils.contract;
 
   await TestProvider.fromContracts(coreContracts, clarityBin);
   await TestProvider.fromContracts(arkadikoContracts, clarityBin);
