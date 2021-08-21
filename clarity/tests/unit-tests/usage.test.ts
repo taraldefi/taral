927b370c3af412233a0c1e330171c8cca607754d
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-21 11:48:46 +0300

    feat(unit-tests): add proof that we can use the contract without deploying it, if already deployed ofc

---
+++ clarity/tests/unit-tests/usage.test.ts
@@ -0,0 +1,19 @@
+import { NO_METADATA, TestProvider } from "../../lib/providers";
+import { taralCoinInfo } from "../../generated/taral/taral-coin";
+import { clarityBin } from "./jest-setup";
+
+test("Test that we can access the token without deploying", async () => {
+  var contractInfo = await TestProvider.fromContracts(
+    false,
+    {
+      taralCoinInfo,
+    },
+    clarityBin
+  );
+
+  var contract = contractInfo.taralCoinInfo.contract;
+
+  const result = (await contract.getDecimals(NO_METADATA))._unsafeUnwrap();
+
+  expect(result).toEqual(6);
+});
