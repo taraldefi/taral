8f6819397508fa99cb07d6503c48d78a904f391b
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-08-23 14:00:47 +0300

    feat(tests): work on bitcoin faucet tests

--- clarity/tests/integration-tests/bitcoin-faucet.test.ts
+++ clarity/tests/integration-tests/bitcoin-faucet.test.ts
@@ -1,48 +1,44 @@
-// import {
-//   FaucetsApi,
-//   Configuration,
-// } from "@stacks/blockchain-api-client";
-// import { StacksNetwork } from "@stacks/network";
-// import { TransactionVersion } from "@stacks/transactions";
-// import crossfetch from "cross-fetch";
-// import { NETWORK } from "../../configuration";
+import {
+  FaucetsApi,
+  Configuration,
+  RunFaucetResponse,
+} from "@stacks/blockchain-api-client";
+import { StacksNetwork } from "@stacks/network";
+import { TransactionVersion } from "@stacks/transactions";
+import crossfetch from "cross-fetch";
+import { NETWORK } from "../../configuration";
 
-// test("Request btc from faucet", async () => {
-//   const apiConfig = new Configuration({
-//     fetchApi: crossfetch,
-//     basePath: NETWORK.coreApiUrl,
-//   });
+test("Request btc from faucet", async () => {
+  const apiConfig = new Configuration({
+    fetchApi: crossfetch,
+    basePath: NETWORK.coreApiUrl,
+  });
 
-//   const faucets = new FaucetsApi(apiConfig);
+  const faucets = new FaucetsApi(apiConfig);
 
-//   return faucets
-//     .runFaucetStx({ address: 'msjUR7JMe3UBqm9b28CKySX6kSdXRguC8g' })
-//     .then((faucetTx: any) => {
-//       return JSONStringify({
-//         txid: faucetTx.txId!,
-//         transaction: generateExplorerTxPageUrl(
-//           faucetTx.txId!.replace(/^0x/, ''),
-//           NETWORK
-//         ),
-//       });
-//     })
-//     .catch((error: any) => error.toString());
-// });
+  var faucetTransaction: RunFaucetResponse = await faucets.runFaucetBtc({
+    address: "mqVnk6NPRdhntvfm4hh9vvjiRkFDUuSYsH",
+  });
 
-// export function generateExplorerTxPageUrl(txid: string, network: StacksNetwork): string {
-//     if (network.version === TransactionVersion.Testnet) {
-//       return `https://explorer.stacks.co/txid/0x${txid}?chain=testnet`;
-//     } else {
-//       return `https://explorer.stacks.co/txid/0x${txid}?chain=mainnet`;
-//     }
-//   }
+  var transactionID = faucetTransaction.txId!;
 
-// type AnyJson = string | number | boolean | null | { [property: string]: AnyJson } | AnyJson[];
+  var transaction = generateExplorerTxPageUrl(
+    faucetTransaction.txId!.replace(/^0x/, ""),
+    NETWORK
+  );
 
-// function JSONStringify(obj: AnyJson, stderr: boolean = false): string {
-//     if ((!stderr && process.stdout.isTTY) || (stderr && process.stderr.isTTY)) {
-//       return JSON.stringify(obj, null, 2);
-//     } else {
-//       return JSON.stringify(obj);
-//     }
-//   }
+  console.log(`Transaction ID ${transactionID}`);
+
+  console.log(`Transaction URL ${transaction}`);
+});
+
+export function generateExplorerTxPageUrl(
+  txid: string,
+  network: StacksNetwork
+): string {
+  if (network.version === TransactionVersion.Testnet) {
+    return `https://explorer.stacks.co/txid/0x${txid}?chain=testnet`;
+  } else {
+    return `https://explorer.stacks.co/txid/0x${txid}?chain=mainnet`;
+  }
+}
