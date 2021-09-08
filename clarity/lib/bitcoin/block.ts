c86c08cfafb775ce38cce451064e7d07691a3d0d
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-09-08 19:18:54 +0300

    feat(block): more typings to also hold the transactions

--- clarity/lib/bitcoin/block.ts
+++ clarity/lib/bitcoin/block.ts
@@ -3,7 +3,7 @@ import { RPCClient } from "rpc-bitcoin";
 export async function getBlockByHash(rpcClient: RPCClient, blockHash: string): Promise<Block> {
     var blockByHash = await rpcClient.getblock({
         blockhash: blockHash,
-        verbosity: 1
+        verbosity: 2
     });
 
     return blockByHash;
@@ -18,16 +18,53 @@ export async function getBlockHeader(rpcClient: RPCClient, blockHash: string): P
     return blockHeader;
 }
 
+export interface ScriptSig {
+    asm: string;
+    hex: string;
+}
+export interface Vin {
+    coinbase: string;
+    txinwitness: string[];
+    sequence: any;
+    txid: string;
+    vout?: number;
+    scriptSig: ScriptSig;
+}
+export interface ScriptPubKey {
+    asm: string;
+    hex: string;
+    reqSigs: number;
+    type: string;
+    addresses: string[];
+}
+export interface Vout {
+    value: number;
+    n: number;
+    scriptPubKey: ScriptPubKey;
+}
+export interface Tx {
+    txid: string;
+    hash: string;
+    version: number;
+    size: number;
+    vsize: number;
+    weight: number;
+    locktime: number;
+    vin: Vin[];
+    vout: Vout[];
+    hex: string;
+}
 export interface Block {
     hash: string;
     confirmations: number;
-    size: number;
     strippedsize: number;
+    size: number;
     weight: number;
     height: number;
     version: number;
     versionHex: string;
     merkleroot: string;
+    tx: Tx[];
     time: number;
     mediantime: number;
     nonce: number;
