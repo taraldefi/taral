98a52a4cee198829d8f7769aac3dd62d283530a2
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-09-16 09:40:10 +0300

    chore(native-cli): integrate native-cli install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ echo "Installing packages "
 
 echo "Using branch develop for the clarity-cli"
 
-export BLOCKSTACK_CORE_SOURCE_BRANCH=develop && yarn
+export yarn && yarn install-native-cli
 
 echo "Done"
