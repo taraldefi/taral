0a22ab0f68ca3bb36b84db7e9d7bdcf03fc658a3
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-09-17 12:56:25 +0300

    feat(tests): delete old generated boot contracts

--- packages/shared/test-utils/contracts/generated/test-utils/test-utils/types.ts
+++
@@ -1,7 +0,0 @@
-import { Transaction } from "../../../../../transaction";
-
-export interface TestUtilsContract {
-  mineBlock: () => Transaction<boolean, null>;
-  getBlockHeight: () => Promise<bigint>;
-  getStxBalance: (account: string) => Promise<bigint>;
-}
