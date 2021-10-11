f406d41ffe41da82d210179ba92ccaae3dcb07d1
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2021-10-11 19:14:18 +0300

    chore(web): work on configuring the react testbed application

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  AppConfig,
+  UserData,
+  showConnect,
+  UserSession,
+} from "@stacks/connect";
+import { useState } from "react";
+import { SimpleStacksWebProvider } from "lib-web";
+import { webTaralContracts } from "taral-contracts";
+import { NETWORK } from "taral-configuration";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const appConfig = new AppConfig();
+const userSession = new UserSession({appConfig});
+
+async function callGetName() {
+  const webProvider = SimpleStacksWebProvider.fromContracts(webTaralContracts, {
+    appDetails: {
+      icon: "https://webpack.js.org/site-logo.1fcab817090e78435061.svg",
+      name: 'Testing'
+    },
+    network: NETWORK,
+    stxAddress: ''
+  });
+
+  const name = (await webProvider.webTaralCoin.contract().getName())._unsafeUnwrap();
+  console.log(name);
 }
 
+function App() {
+  
+  const [user, setUser] = useState<UserData>();
+
+  if (user || userSession.isUserSignedIn()) {
+    return (
+      <div>
+        <button
+          onClick={async () => await callGetName()}
+        >
+          Call
+        </button>
+        <button
+          onClick={() => {
+            userSession.signUserOut();
+            setUser(undefined);
+          }}
+        >
+          Disconnect
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button
+          onClick={() =>
+            showConnect({
+              appDetails: {
+                name: "Webflow",
+                icon: "https://webpack.js.org/site-logo.1fcab817090e78435061.svg",
+              },
+              onFinish: () => {
+                setUser(userSession.loadUserData());
+              },
+            })
+          }
+        >
+          Connect with Stacks Wallet
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
