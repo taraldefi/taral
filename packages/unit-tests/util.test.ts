e84dd1df527a75732b63def627844e23e1240247
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-03-08 17:09:55 +0200

    fix(tests): fix util test

--- packages/unit-tests/util.test.ts
+++ packages/unit-tests/util.test.ts
@@ -2,8 +2,7 @@ import { getBlockHeight, mineBlocks } from "lib-testing";
 import { clarityBin } from "./jest-setup";
 
 test("Can get block height and mine blocks", async () => {
-  const startHeight = await getBlockHeight(clarityBin);
-  expect(startHeight).toEqual(54n);
+  const startHeight: number = await getBlockHeight(clarityBin);
   await mineBlocks(5, clarityBin);
-  expect(await getBlockHeight(clarityBin)).toEqual(59n);
+  expect(await getBlockHeight(clarityBin)).toEqual(BigInt(startHeight) + BigInt(5));
 }, 3000000);
