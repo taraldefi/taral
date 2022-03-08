2b6999c4d855f95c77b84b77f25a564004180321
dorucioclea <dorucioclea@users.noreply.github.com>
2022-03-08 15:51:07 +0000

    style(formatting): apply formatting changes

--- packages/unit-tests/oracle.test.ts
+++ packages/unit-tests/oracle.test.ts
@@ -1,39 +1,42 @@
 import { clarinetAccounts, taralOracle } from "./jest-setup";
 import { ClarinetAccount, txErr, txOk } from "lib-shared";
 
-const UnauthorizedAddress: string = 'ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG';
+const UnauthorizedAddress: string = "ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG";
 const UnauthorizedClarinetAccount: ClarinetAccount = {
-    address: UnauthorizedAddress,
-    mnemonic: '',
-    privateKey: '',
-    balance: BigInt(0)
+  address: UnauthorizedAddress,
+  mnemonic: "",
+  privateKey: "",
+  balance: BigInt(0),
 };
 
 test("Can set price", async () => {
-    const token = taralOracle(clarinetAccounts.deployer);
-    const result = await txOk(token.setOracleOwner(clarinetAccounts.deployer.address));
-    expect(result.value).toBe(true);
+  const token = taralOracle(clarinetAccounts.deployer);
+  const result = await txOk(
+    token.setOracleOwner(clarinetAccounts.deployer.address)
+  );
+  expect(result.value).toBe(true);
 }, 3000000);
 
 test("Cannot set price", async () => {
-    const token = taralOracle(UnauthorizedClarinetAccount);
-    const result = await txErr(token.setOracleOwner(UnauthorizedAddress));
-    expect(result.value).toBe(8401n);
+  const token = taralOracle(UnauthorizedClarinetAccount);
+  const result = await txErr(token.setOracleOwner(UnauthorizedAddress));
+  expect(result.value).toBe(8401n);
 }, 3000000);
 
 test("Can update price", async () => {
-    const token = taralOracle(clarinetAccounts.deployer);
-    const result = await txOk(token.setOracleOwner(clarinetAccounts.deployer.address));
-    expect(result.value).toBe(true);
-    const priceUpdateResult = await txOk(token.updatePrice('STX', 2n, 2));
-    expect(priceUpdateResult.value).toBe(2n);
-    var getPriceResult = await token.getPrice('STX');
-    expect(getPriceResult["last-price"]).toBe(2n);
+  const token = taralOracle(clarinetAccounts.deployer);
+  const result = await txOk(
+    token.setOracleOwner(clarinetAccounts.deployer.address)
+  );
+  expect(result.value).toBe(true);
+  const priceUpdateResult = await txOk(token.updatePrice("STX", 2n, 2));
+  expect(priceUpdateResult.value).toBe(2n);
+  var getPriceResult = await token.getPrice("STX");
+  expect(getPriceResult["last-price"]).toBe(2n);
 }, 3000000);
 
 test("Cannot update price", async () => {
-    const token = taralOracle(UnauthorizedClarinetAccount);
-    const priceUpdateResult = await txErr(token.updatePrice('STX', 2n, 2));
-    expect(priceUpdateResult.value).toBe(851n);
+  const token = taralOracle(UnauthorizedClarinetAccount);
+  const priceUpdateResult = await txErr(token.updatePrice("STX", 2n, 2));
+  expect(priceUpdateResult.value).toBe(851n);
 }, 3000000);
-
--- packages/unit-tests/util.test.ts
+++ packages/unit-tests/util.test.ts
@@ -4,5 +4,7 @@ import { clarityBin } from "./jest-setup";
 test("Can get block height and mine blocks", async () => {
   const startHeight: number = await getBlockHeight(clarityBin);
   await mineBlocks(5, clarityBin);
-  expect(await getBlockHeight(clarityBin)).toEqual(BigInt(startHeight) + BigInt(5));
+  expect(await getBlockHeight(clarityBin)).toEqual(
+    BigInt(startHeight) + BigInt(5)
+  );
 }, 3000000);
