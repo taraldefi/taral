d5ce4a00d02da6b6d59ed6c6a0501ca72fac712e
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-03-27 18:16:15 +0300

    feat(oracle): add taral safe oracle implementation

--- packages/clarity/contracts/taral/taral-oracle-trait-v1.clar
+++
@@ -1,5 +0,0 @@
-(define-trait oracle-trait
-  (
-    (fetch-price ((string-ascii 12)) (response (tuple (last-price uint) (last-block uint) (decimals uint)) uint))
-  )
-)
--- packages/scripts/testbed.ts
+++ packages/scripts/testbed.ts
@@ -1,7 +1,8 @@
 import * as btc from "bitcoinjs-lib";
 import { address } from "bitcoinjs-lib";
-import { decodeBtcAddress } from "lib-bitcoin";
-import { Logger } from "lib-shared";
+import { decodeBtcAddress, decodeStxAddress } from "lib-bitcoin";
+import { getClarinetAccounts } from "lib-infra";
+import { getRootDirectory, Logger } from "lib-shared";
 
 const NAME = "Testbed";
 
@@ -11,23 +12,31 @@ async function main() {
   console.log("A place to quickly prototype and test things.");
   console.log(`Ran from ${cwd}`);
 
-  // const root = `${getRootDirectory()}/packages/clarity`;
-  // const contracts = await getClarinetAccounts(root);
-  // Logger.debug(NAME, "Contracts are:", contracts);
-  const result = decodeBtcAddress("mqVnk6NPRdhntvfm4hh9vvjiRkFDUuSYsH");
+  const root = `${getRootDirectory()}/packages/clarity`;
+  const contracts = await getClarinetAccounts(root);
+  Logger.debug(NAME, "Contracts are:", contracts);
+  // const result = decodeBtcAddress("mqVnk6NPRdhntvfm4hh9vvjiRkFDUuSYsH");
+
+  // Logger.debug(
+  //   NAME,
+  //   "Decoded btc address: ",
+  //   `0x${result.data.toString("hex")}`
+  // );
+  // Logger.debug("Decoded btc raw response", JSON.stringify(result));
+
+  // const outputScript = address.toOutputScript(
+  //   "mqVnk6NPRdhntvfm4hh9vvjiRkFDUuSYsH",
+  //   btc.networks.regtest
+  // );
+  // Logger.debug(NAME, "Output script is:", outputScript.toString("hex"));
+
+  const result = decodeStxAddress("ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM");
 
   Logger.debug(
     NAME,
-    "Decoded btc address: ",
-    `0x${result.data.toString("hex")}`
+    "Decoded stx address: ",
+    `0x${result.data.toString("hex")}` // 0x6d78de7b0625dfbfc16c3a8a5735f6dc3dc3f2ce
   );
-  Logger.debug("Decoded btc raw response", JSON.stringify(result));
-
-  const outputScript = address.toOutputScript(
-    "mqVnk6NPRdhntvfm4hh9vvjiRkFDUuSYsH",
-    btc.networks.regtest
-  );
-  Logger.debug(NAME, "Output script is:", outputScript.toString("hex"));
 }
 
 main();
