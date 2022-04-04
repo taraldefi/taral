dd82cd9610b73c4c94bdc51390972a54a2eebb7e
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-04-04 11:37:31 +0300

    feat(oracle): revoke source command implementation

---
+++ apps/oracle/src/commands/revoke-source-command.ts
@@ -0,0 +1,11 @@
+import { revokeSource } from "lib-oracle";
+import { ORACLE_HELPER } from "../utils/contract-helper";
+
+export async function revokeSourceCommand() {
+  const contract = await ORACLE_HELPER.buildOracleContract();
+
+  await revokeSource({
+    contract,
+    source: "test1",
+  });
+}
