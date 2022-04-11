bf6c93b410f3a54ab0f73abac166b4ccd14d8cdf
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-04-11 15:35:41 +0300

    feat(oracle): handle commands

--- apps/oracle/src/cli.ts
+++ apps/oracle/src/cli.ts
@@ -1,13 +1,35 @@
 import { Command } from "commander";
+import { addSourceCommand } from "./commands/add-source-command";
+import { checkPricesCommand } from "./commands/check-prices-command";
+import { checkSourcesCommand } from "./commands/check-sources-command";
+import { feedOraclePricesCommand } from "./commands/feed-oracle-prices-command";
+import { revokeSourceCommand } from "./commands/revoke-source-command";
+import { updatePricesCommand } from "./commands/update-prices-command";
 
 const program = new Command();
 
-program.command("hello").action(() => {
-  console.log("hello there");
+program.command("add-source").action(async () => {
+  await addSourceCommand();
 });
 
-program.command("bye").action(() => {
-  console.log("bye there");
+program.command("check-prices").action(async () => {
+  await checkPricesCommand();
+});
+
+program.command("check-sources").action(async () => {
+  await checkSourcesCommand();
+});
+
+program.command("feed-oracle-prices").action(async () => {
+  await feedOraclePricesCommand();
+});
+
+program.command("revoke-source").action(async () => {
+  await revokeSourceCommand();
+});
+
+program.command("update-prices").action(async () => {
+  await updatePricesCommand();
 });
 
 program.parse(process.argv);
