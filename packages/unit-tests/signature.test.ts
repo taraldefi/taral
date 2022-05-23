433c00dfd4907c38ec1e2df135a0d94d4a4db7f0
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-05-23 18:50:58 +0300

    feat(signature): add test that proves signature verification with noble

---
+++ packages/unit-tests/signature.test.ts
@@ -0,0 +1,77 @@
+import {
+  bytesToHex,
+  createStacksPrivateKey,
+  getAddressFromPublicKey,
+  hexToBigInt,
+  parseRecoverableSignature,
+  PubKeyEncoding,
+  publicKeyFromPrivKey,
+  publicKeyFromSignatureVrs,
+  signatureRsvToVrs,
+  signMessageHashRsv,
+  StacksPrivateKey,
+  TransactionVersion,
+  utf8ToBytes,
+  verifyMessageSignatureRsv,
+} from "lib-stacks";
+import { clarinetAccounts } from "./jest-setup";
+import { Signature, verify } from "@noble/secp256k1";
+
+test("Signature verification successful", () => {
+  const message = "Hello world";
+  const deployerPrivateKey = clarinetAccounts.deployer.privateKey;
+  const publicKey = publicKeyFromPrivKey(deployerPrivateKey);
+
+  const stacksPrivateKey: StacksPrivateKey =
+    createStacksPrivateKey(deployerPrivateKey);
+
+  const messageHex = bytesToHex(utf8ToBytes(message));
+
+  const signature = signMessageHashRsv({
+    messageHash: messageHex,
+    privateKey: stacksPrivateKey,
+  });
+
+  const compressedPubKeyFromSig = publicKeyFromSignatureVrs(
+    messageHex,
+    signature,
+    PubKeyEncoding.Compressed
+  );
+
+  expect(publicKeyFromSignatureVrs).toEqual(publicKey);
+
+  const signatureVerificationResult = verifyMessageSignatureRsv({
+    message: Buffer.from(messageHex, "hex"),
+    publicKey: compressedPubKeyFromSig,
+    signature: signature.data,
+  });
+
+  expect(signatureVerificationResult).toBeTruthy();
+
+  const { r, s } = parseRecoverableSignature(signatureRsvToVrs(signature.data));
+
+  const nobleSignature: Signature = new Signature(
+    hexToBigInt(r),
+    hexToBigInt(s)
+  );
+
+  const nobleSignatureVerificationResult = verify(
+    nobleSignature,
+    messageHex,
+    compressedPubKeyFromSig,
+    {
+      strict: true,
+    }
+  );
+
+  expect(nobleSignatureVerificationResult).toBeTruthy();
+
+  const addressFromPublicKey = getAddressFromPublicKey(
+    compressedPubKeyFromSig,
+    TransactionVersion.Testnet
+  );
+
+  expect(addressFromPublicKey).toEqual(clarinetAccounts.deployer.address);
+
+  console.log(JSON.stringify(signature));
+}, 3000000);
