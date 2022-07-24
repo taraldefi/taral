d462025ae6e0f0ace086bba1657b452788bf32f2
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-07-24 19:24:34 +0300

    feat(signature): signature verification

--- standalone/storage-api/src/files/files.on-chain.service.ts
+++ standalone/storage-api/src/files/files.on-chain.service.ts
@@ -12,6 +12,59 @@ import {
   updateAccessToFile,
   updateFile,
 } from '@libs/storage';
+import { SignatureVerificationModel } from './models/signature-verification.model';
+import { getAddressFromPublicKey, hashStacksMessage, hexToBigInt, MessageSignature, parseRecoverableSignature, PubKeyEncoding, publicKeyFromSignatureVrs, signatureRsvToVrs, StacksMessageType, TransactionVersion } from '@libs/stacks';
+import { Signature, verify } from '@noble/secp256k1';
 
 @Injectable()
-export class FilesOnChainService {}
+export class FilesOnChainService {
+
+  verifySignature(signature: string, message: string): SignatureVerificationModel {
+    const messageHex = hashStacksMessage({ message });
+    const { r, s } = parseRecoverableSignature(signatureRsvToVrs(signature));
+
+    const messageSignature: MessageSignature = {
+      data: signature,
+      type: StacksMessageType.MessageSignature
+    };
+
+    const compressedPubKeyFromSig = publicKeyFromSignatureVrs(
+      messageHex,
+      messageSignature,
+      PubKeyEncoding.Compressed
+    );
+
+    const nobleSignature: Signature = new Signature(
+      hexToBigInt(r),
+      hexToBigInt(s)
+    );
+
+    const nobleSignatureVerificationResult = verify(
+      nobleSignature,
+      messageHex,
+      compressedPubKeyFromSig,
+      {
+        strict: true,
+      }
+    );
+
+    if (!nobleSignatureVerificationResult) {
+      return {
+        address: '',
+        publicKey: '',
+        isValid: false
+      }
+    }
+
+    const addressFromPublicKey = getAddressFromPublicKey(
+      compressedPubKeyFromSig,
+      TransactionVersion.Testnet
+    );
+
+    return {
+      address: addressFromPublicKey,
+      isValid: true,
+      publicKey: compressedPubKeyFromSig
+    }
+  }
+}
