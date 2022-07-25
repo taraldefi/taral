dffff0f828f364b1fa244622cc2e0881678f74ee
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-07-25 19:07:05 +0300

    feat(encryption): decrypt method

--- standalone/storage-api/src/files/services/onchain/encryption.service.ts
+++ standalone/storage-api/src/files/services/onchain/encryption.service.ts
@@ -1,9 +1,35 @@
 import { Injectable, NotFoundException, Scope } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import {
+    decryptString,
+    ecPrivateKey,
+    encryptString,
+    getPublicKeyFromPrivate,
+  } from "lib-stacks";
 
 @Injectable({
-  scope: Scope.DEFAULT,
+    scope: Scope.DEFAULT,
 })
 export class EncryptionService {
 
+    private readonly privateKey: string;
 
+    constructor(private configService: ConfigService) {
+
+        this.privateKey = this.configService.get(
+            'onchain.deployerprivatekey',
+        ) as string;
+    }
+
+    public async decrypt(content: Buffer): Promise<Buffer> {
+
+        const stringContent: string = content.toString('utf8');
+
+        const decryptedContent = await decryptString(this.privateKey, stringContent);
+
+        return Buffer.from(decryptedContent, 'utf-8');
+    }
+
+    
 }
