b0989d11cb7917c1e96787d846e42a386de178bf
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-07-25 18:58:01 +0300

    feat(api): implement new small api lib provider

---
+++ libs/api/index.ts
@@ -0,0 +1,2 @@
+export * from "./api-provider";
+export * from "./stacks/deploy-contract";
---
+++ libs/api/stacks/deploy-contract.ts
@@ -0,0 +1,30 @@
+import { StacksNetwork } from "@stacks/network";
+import { makeContractDeploy } from "@stacks/transactions";
+import * as fs from "fs";
+import { Logger } from "lib-shared";
+import { handleTransaction } from "lib-stacks";
+
+const NAME = "contract-deployer";
+
+export async function deployContractOnStacks(
+  contractName: string,
+  contractPath: string,
+  network: StacksNetwork,
+  secretDeployKey: string
+) {
+  Logger.debug(NAME, `preparing to deploy contract ${contractName} ......`);
+
+  const codeBody = fs.readFileSync(contractPath).toString();
+
+  const transaction = await makeContractDeploy({
+    contractName,
+    codeBody,
+    senderKey: secretDeployKey,
+    network,
+    anchorMode: 3,
+  });
+
+  Logger.debug(NAME, `contract deploy successful - ${contractName}`);
+
+  return handleTransaction(transaction, network);
+}
