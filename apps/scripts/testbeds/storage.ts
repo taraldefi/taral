85a3ed022c0c8b22a4feba919b56f204fa9bdd99
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-07-27 10:48:23 +0300

    feat(storage): request file

--- apps/scripts/testbeds/storage.ts
+++ apps/scripts/testbeds/storage.ts
@@ -1,15 +1,52 @@
 import fetch from "node-fetch";
-import { CreateFileResponse } from "./storage/models";
-import { createFormPayload } from "./storage/create-file-payload";
+import { CreateFileResponse, RequestFileResponse } from "./storage/models";
+import { createFormPayload } from './storage/create-file-payload';
+import { sign } from "./storage/signature-payload";
 
 export async function storageManualTest() {
-  const response = await createFile();
+    const response = await createFile();
 
-  if (response == null) {
-    console.log("Errored out");
-  } else {
-    console.log("Success", JSON.stringify(response));
-  }
+    if (response == null) {
+        console.log('Errored out');
+    } else {
+        console.log('Success', JSON.stringify(response));
+    }
+
+    const fileResponse = await requestFile(response!.id);
+    
+    if (response == null) {
+        console.log('Errored out');
+    } else {
+        console.log('Success', JSON.stringify(fileResponse));
+    }
+}
+
+export async function requestFile(id: number): Promise<RequestFileResponse | null> {
+    const signature = sign();
+
+    const body = {
+        externalId: String(id),
+        signedMessage: signature.message,
+        signature: signature.data
+    };
+
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    }
+
+    try {
+        const response = await fetch(`http://localhost:3000/api/v1/files/request-file`, requestOptions);
+
+        const result = await response.json() as RequestFileResponse;
+
+        return result;
+    } catch (error) {
+        console.log("Error ", error);
+
+        return null;
+    }
 }
 
 export async function createFile(): Promise<CreateFileResponse | null> {
--- apps/scripts/testbeds/storage/models.ts
+++ apps/scripts/testbeds/storage/models.ts
@@ -1,6 +1,15 @@
 export interface CreateFileResponse {
-  hash: string;
-  id: number;
-  name: string;
-  signedHash: string;
+    hash: string;
+    id: number;
+    name: string;
+    signedHash: string;
+}
+
+export interface RequestFileResponse {
+    iv: string;
+    ephemeralPK: string;
+    cipherText: string;
+    mac: string;
+    wasString: boolean;
+    cipherTextEncoding: string;
 }
