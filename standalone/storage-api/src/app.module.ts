0ae1fc7843641f80db89f36d5ba3e4297d4a5c13
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-08-01 18:20:40 +0300

    feat: diisable i18n for now

--- standalone/storage-api/src/app.module.ts
+++ standalone/storage-api/src/app.module.ts
@@ -4,13 +4,13 @@ import authConfig from './config/auth.config';
 import appConfig from './config/app.config';
 import mailConfig from './config/mail.config';
 import fileConfig from './config/file.config';
-import * as path from 'path';
+// import * as path from 'path';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { I18nModule } from 'nestjs-i18n/dist/i18n.module';
-import { I18nJsonParser } from 'nestjs-i18n/dist/parsers/i18n.json.parser';
-import { HeaderResolver } from 'nestjs-i18n';
+// import { I18nModule } from 'nestjs-i18n/dist/i18n.module';
+// import { I18nJsonParser } from 'nestjs-i18n/dist/parsers/i18n.json.parser';
+// import { HeaderResolver } from 'nestjs-i18n';
 import { TypeOrmConfigService } from './database/typeorm-config.service';
 import { MailConfigService } from './mail/mail-config.service';
 import { HomeModule } from './home/home.module';
@@ -56,22 +56,22 @@ const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
     MailerModule.forRootAsync({
       useClass: MailConfigService,
     }),
-    I18nModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        fallbackLanguage: configService.get('app.fallbackLanguage'),
-        parserOptions: {
-          path: path.join(
-            configService.get('app.workingDirectory'),
-            'src',
-            'i18n',
-            'translations',
-          ),
-        },
-      }),
-      parser: I18nJsonParser,
-      inject: [ConfigService],
-      resolvers: [new HeaderResolver(['x-custom-lang'])],
-    }),
+    // I18nModule.forRootAsync({
+    //   useFactory: (configService: ConfigService) => ({
+    //     fallbackLanguage: configService.get('app.fallbackLanguage'),
+    //     parserOptions: {
+    //       path: path.join(
+    //         configService.get('app.workingDirectory'),
+    //         'src',
+    //         'i18n',
+    //         'translations',
+    //       ),
+    //     },
+    //   }),
+    //   parser: I18nJsonParser,
+    //   inject: [ConfigService],
+    //   resolvers: [new HeaderResolver(['x-custom-lang'])],
+    // }),
     StorageModule.registerAsync({
       imports: [ConfigService],
       useFactory: (config: ConfigService) => {
