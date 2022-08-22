dc47e07fb88741639df287926f2edf4fc37a6800
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-08-22 18:50:03 +0300

    feat: business api

---
+++ standalone/business-api/src/app.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import mailConfig from './config/mail.config';
+import fileConfig from './config/file.config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { MailConfigService } from './mail/mail-config.service';
+import { HomeModule } from './home/home.module';
+import { EventModule } from '@modules/events';
+import { StorageModule } from '@modules/storage';
+import { LoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        appConfig,
+        mailConfig,
+        fileConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    MailerModule.forRootAsync({
+      useClass: MailConfigService,
+    }),
+    StorageModule.registerAsync({
+      imports: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return config.get('filesystem');
+      },
+      inject: [ConfigService],
+    }),
+    HomeModule,
+    EventModule,
+    StorageModule,
+    LoggerModule,
+  ],
+})
+export class AppModule {}
