6686ec958be806f670774eb9475e3a44d3fe6d00
Cioclea Doru Octavian <39082034+dorucioclea@users.noreply.github.com>
2022-08-23 12:59:51 +0300

    feat(entity): controller

--- standalone/business-api/src/entity/legal-entities.controller.ts
+++ standalone/business-api/src/entity/legal-entities.controller.ts
@@ -1,5 +1,9 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { FormDataRequest, MemoryStoredFile } from "@modules/multipart";
+import { Body, Controller, Delete, Param, Post } from "@nestjs/common";
+import { ApiBody, ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { CreateEntityDto } from "./dto/request/create-entity.dto";
+import { UpdateEntityDto } from "./dto/request/update-entity.dto";
+import { GetEntityDetailsResponse } from "./dto/response/get-entity-details-response.dto";
 import { EntityService } from "./services/entity.service";
 
 @ApiTags('Entities')
@@ -11,4 +15,129 @@ export class EntityController {
   constructor(
     private readonly entityService: EntityService,
   ) {}
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        logo: {
+          type: 'string',
+          format: 'binary',
+        },
+
+        name: {
+          type: 'string',
+          format: 'string'
+        },
+
+        beneficialOwner: {
+          type: 'string',
+          format: 'string'
+        },
+
+        abbreviation: {
+          type: 'string',
+          format: 'string'
+        },
+
+        nationality: {
+          type: 'string',
+          format: 'string'
+        },
+
+        headquarters: {
+          type: 'string',
+          format: 'string'
+        },
+
+        industryType: {
+          type: 'string',
+          format: 'string'
+        },
+
+        coreBusiness: {
+          type: 'string',
+          format: 'string'
+        },
+
+        incorporationDate: {
+          type: 'string',
+          format: 'date'
+        },
+
+        legalForm: {
+          type: 'string',
+          format: 'string'
+        },
+      },
+    },
+  })
+  @FormDataRequest({ storage: MemoryStoredFile })
+  @Post('create')
+  async createEntity( @Body() entity: CreateEntityDto ): Promise<GetEntityDetailsResponse> {
+    return await this.entityService.createEntity(entity);
+  }
+
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string',
+          format: 'string'
+        },
+
+        beneficialOwner: {
+          type: 'string',
+          format: 'string'
+        },
+
+        abbreviation: {
+          type: 'string',
+          format: 'string'
+        },
+
+        nationality: {
+          type: 'string',
+          format: 'string'
+        },
+
+        headquarters: {
+          type: 'string',
+          format: 'string'
+        },
+
+        industryType: {
+          type: 'string',
+          format: 'string'
+        },
+
+        coreBusiness: {
+          type: 'string',
+          format: 'string'
+        },
+
+        incorporationDate: {
+          type: 'string',
+          format: 'date'
+        },
+
+        legalForm: {
+          type: 'string',
+          format: 'string'
+        },
+      },
+    },
+  })
+
+  @Post('update/:id')
+  async updateEntity(@Param('id') id,  @Body() entity: UpdateEntityDto): Promise<GetEntityDetailsResponse> {
+    return await this.entityService.updateEntity(id, entity);
+  }
+
+  @Delete('delete/:id')
+  async deleteEntity(@Param('id') id) {
+    await this.entityService.deleteEntity(id);
+  }
 }
