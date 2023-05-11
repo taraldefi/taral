384c27b79c31a6d617686b0898c2215a61223263
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-05-11 18:10:32 +0300

    feat: seeds

---
+++ standalone/portal-api/src/database/seeds/create-permission.seed.ts
@@ -0,0 +1,66 @@
+import { Factory } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+
+import {
+  ModulesPayloadInterface,
+  PermissionConfiguration,
+  PermissionPayload,
+  RoutePayloadInterface,
+  SubModulePayloadInterface
+} from 'src/config/permission.config';
+import { PermissionEntity } from 'src/modules/permission/entities/permission.entity';
+
+export default class CreatePermissionSeed {
+  permissions: RoutePayloadInterface[] = [];
+
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    const modules = PermissionConfiguration.modules;
+    for (const moduleData of modules) {
+      let resource = moduleData.resource;
+      this.assignResourceAndConcatPermission(moduleData, resource);
+
+      if (moduleData.hasSubmodules) {
+        for (const submodule of moduleData.submodules) {
+          resource = submodule.resource || resource;
+          this.assignResourceAndConcatPermission(submodule, resource);
+        }
+      }
+    }
+
+    if (this.permissions && this.permissions.length > 0) {
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(PermissionEntity)
+        .values(this.permissions)
+        .orIgnore()
+        .execute();
+    }
+  }
+
+  assignResourceAndConcatPermission(
+    modules: ModulesPayloadInterface | SubModulePayloadInterface,
+    resource: string,
+    isDefault?: false
+  ) {
+    if (modules.permissions) {
+      for (const permission of modules.permissions) {
+        this.concatPermissions(permission, resource, isDefault);
+      }
+    }
+  }
+
+  concatPermissions(
+    permission: PermissionPayload,
+    resource: string,
+    isDefault: boolean
+  ) {
+    const description = permission.name;
+    for (const data of permission.route) {
+      data.resource = data.resource || resource;
+      data.description = data.description || description;
+      data.isDefault = isDefault;
+    }
+    this.permissions = this.permissions.concat(permission.route);
+  }
+}
---
+++ standalone/portal-api/src/database/seeds/create-role.seed.ts
@@ -0,0 +1,36 @@
+import { Factory } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+
+import { RoleEntity } from 'src/modules/role/entities/role.entity';
+import { PermissionConfiguration } from 'src/config/permission.config';
+import { PermissionEntity } from 'src/modules/permission/entities/permission.entity';
+
+export default class CreateRoleSeed {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    const roles = PermissionConfiguration.roles;
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(RoleEntity)
+      .values(roles)
+      .orIgnore()
+      .execute();
+
+    // Assign all permission to superUser
+    const role = await connection
+      .getRepository(RoleEntity)
+      .createQueryBuilder('role')
+      .where('role.name = :name', {
+        name: 'superuser'
+      })
+      .getOne();
+
+    if (role) {
+      role.permission = await connection
+        .getRepository(PermissionEntity)
+        .createQueryBuilder('permission')
+        .getMany();
+      await role.save();
+    }
+  }
+}
