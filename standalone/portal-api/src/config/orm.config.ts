43472e432f95eba1de0720a39b440bb374b7a96f
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-05-11 18:24:14 +0300

    feat: orm config

---
+++ standalone/portal-api/src/config/orm.config.ts
@@ -0,0 +1,25 @@
+import { ConnectionOptions } from 'typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('database');
+const ormConfig: ConnectionOptions = {
+  type: process.env.DATABASE_TYPE || dbConfig.DATABASE_TYPE,
+  host: process.env.DATABASE_HOST || dbConfig.DATABASE_HOST,
+  port: process.env.DATABASE_PORT || dbConfig.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME || dbConfig.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD || dbConfig.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME || dbConfig.DATABASE_NAME,
+  migrationsTransactionMode: 'each',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  logging: false,
+  synchronize: false,
+  migrationsRun: process.env.NODE_ENV === 'test',
+  dropSchema: process.env.NODE_ENV === 'test',
+  migrationsTableName: 'migrations',
+  migrations: [__dirname + '/../database/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/database/migrations'
+  }
+};
+
+export = ormConfig;
