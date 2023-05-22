c8dd6f6e0840e34dfba6b871c958ab508c0b8e0b
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-05-22 22:36:36 +0300

    chore: delete transaction module

--- standalone/modules/transaction/BaseRepository.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectLiteral, Repository } from "typeorm";
-import { patchRepositoryManager } from "./patch-typeorm-repository";
-
-export class BaseRepository<
-  Entity extends ObjectLiteral
-> extends Repository<Entity> {}
-
-patchRepositoryManager(BaseRepository.prototype);
--- standalone/modules/transaction/TransactionalError.ts
+++
@@ -1,7 +0,0 @@
-export class TransactionalError extends Error {
-  public name = "TransactionalError";
-  constructor(message: string) {
-    super(message);
-    Object.setPrototypeOf(this, TransactionalError.prototype);
-  }
-}
