c5d1a58d82249f2e98e87dd66ade3560f097e887
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-09-19 14:11:18 +0300

    feat(buyers): implement buyer creation

--- standalone/portal-api/src/modules/buyer/services/buyer.service.ts
+++ standalone/portal-api/src/modules/buyer/services/buyer.service.ts
@@ -5,16 +5,34 @@ import { BuyerEntity } from "../models/buyer.entity";
 import { triggerError } from "src/modules/entity/utils/trigger.error";
 import { GetBuyerResponse } from "../dto/response/get-buyer-response.dto";
 import { EntityMappingService } from "./mapping.service";
+import { IsolationLevel, Transactional } from "src/common/transaction";
+import { CreateBuyerRequest } from "../dto/request/create-buyer-request.dto";
+import { BaseService } from "src/modules/auctionhistory/services/base.service";
+import { BuyerCompanyEntity } from "src/modules/company/models/buyer.company.entity";
+import { CompanyAddressEntity } from "src/modules/company/models/company.address.entity";
+import { BuyerCompanyEntityRepository } from "../repositories/buyer-company.repository";
+import { SectorEntityRepository } from "../repositories/sector.repository";
+import { CompanyAddressEntityRepository } from "../repositories/company-address.repository";
+import { SectorEntity } from "src/modules/sectors/models/sector.entity";
 
 @Injectable()
-export class BuyerService {
+export class BuyerService extends BaseService {
  constructor(
     @InjectRepository(BuyerEntity)
     private buyerEntityRepository: BuyerEntityRepository,
 
+    @InjectRepository(CompanyAddressEntity)
+    private companyAddressRepository: CompanyAddressEntityRepository,
+
+    @InjectRepository(BuyerCompanyEntity)
+    private buyerCompanyRepository: BuyerCompanyEntityRepository,
+
+    @InjectRepository(SectorEntityRepository)
+    private sectorEntityRepository: SectorEntityRepository,
+
     private mappingService: EntityMappingService,
  ) {
-
+  super();
  }
 
  public async deleteBuyer(id: string): Promise<void> {
@@ -41,5 +59,46 @@ export class BuyerService {
     if (!entity) throw triggerError('entity-not-found');
 
     return this.mappingService.mapEntityDetails(entity);
+  }
+
+  @Transactional({
+    isolationLevel: IsolationLevel.READ_COMMITTED,
+  })
+  public async createEntity(
+    data: CreateBuyerRequest,
+  ): Promise<GetBuyerResponse> {
+    this.setupTransactionHooks();
+
+    const entity = new BuyerEntity();
+    const company = new BuyerCompanyEntity();
+
+    const address = new CompanyAddressEntity();
+    entity.company.address.addressLine1 = data.company.address.addressLine1;
+    entity.company.address.addressLine2 = data.company.address.addressLine2;
+    entity.company.address.city = data.company.address.city;
+    entity.company.address.postalCode = data.company.address.postalCode;
+
+    var addressSavedResult = await this.companyAddressRepository.save(address);
+
+    company.address = addressSavedResult;
+    company.companyName = data.company.companyName;
+    company.dateEstablished = data.company.dateEstablished;
+    company.employeeCount = data.company.employeeCount;
+    company.registrationNumbers = data.company.registrationNumbers;
+
+    var companySavedResult = await this.buyerCompanyRepository.save(company);
+
+    const sector = new SectorEntity();
+    sector.industryType = data.sector.industryType;
+    sector.status = data.sector.status;
+
+    var sectorSavedResult = await this.sectorEntityRepository.save(sector);
+
+    entity.company = companySavedResult;
+    entity.sector = sectorSavedResult;
+
+    var entitySavedResult = await this.buyerEntityRepository.save(entity);
+
+    return this.mappingService.mapEntityDetails(entitySavedResult);
   }
 }
