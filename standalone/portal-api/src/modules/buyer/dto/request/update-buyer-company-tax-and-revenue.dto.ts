3449728efcac02ec5c0373c9c554b4d6e6f735fd
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-09-19 14:13:41 +0300

    feat(buyers): update dtos

---
+++ standalone/portal-api/src/modules/buyer/dto/request/update-buyer-company-tax-and-revenue.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsDateString, IsNotEmpty, IsNumberString, IsOptional, IsString } from "class-validator";
+
+export class UpdateBuyerCompanyTaxAndRevenueRequest {
+    
+    @ApiProperty({ example: 'Engelbrecht Ltd' })
+    @IsOptional()
+    @IsString()
+    taxNumber?: string;
+  
+    @ApiProperty({ example: '12-12-2022' })
+    @IsDateString()
+    lastFiscalYear?: Date;
+  
+    @ApiProperty({ example: '1000000000' })
+    @IsNotEmpty()
+    @IsNumberString()
+    totalRevenue: string;
+  
+    @ApiProperty({ example: '1000000000' })
+    @IsNumberString()
+    exportValue?: number;
+  
+
+    @ApiProperty({ example: '1000000000' })
+    @IsNotEmpty()
+    @IsBoolean()
+    audited: boolean;
+  
+    @ApiProperty({ example: '15' })
+    @IsNotEmpty()
+    @IsNumberString()
+    exportRevenuePercentage: number;
+}
