85ad65bd343d4484de542b2ac3cd11a7232945f2
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-09-30 10:22:56 +0300

    feat(suppliers): configure the supplier module

--- standalone/portal-api/src/modules/supplier/suppliers.module.ts
+++ standalone/portal-api/src/modules/supplier/suppliers.module.ts
@@ -6,11 +6,18 @@ import { CompaniesModule } from '../company/company.module';
 import { SuppliersEntityController } from './suppliers.controller';
 import { SupplierService } from './services/supplier.service';
 import { EntityMappingService } from './services/mapping.service';
+import { SupplierFinancialInformationEntity } from '../financial/models/supplier.financial.info.entity';
+import { CompanyTaxAndRevenueEntity } from '../company/models/company.tax.and.revenue.entity';
+import { SupplierRatingEntity } from '../rating/models/supplier.rating.entity';
 
 @Module({
   imports: [
     CompaniesModule,
-    TypeOrmModule.forFeature([SupplierEntity])
+    TypeOrmModule.forFeature([SupplierEntity]),
+    TypeOrmModule.forFeature([SupplierFinancialInformationEntity]),
+    TypeOrmModule.forFeature([CompanyTaxAndRevenueEntity]),
+    TypeOrmModule.forFeature([SupplierRatingEntity]),
+
   ],
   controllers: [SuppliersEntityController],
   providers: [ConfigModule, ConfigService, SupplierService, EntityMappingService],
