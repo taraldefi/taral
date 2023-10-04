32f1e2bd99dab79916ed2f3c5fad9aa07186065e
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-10-04 12:19:45 +0300

    feat(relationship): import modules

--- standalone/portal-api/src/modules/relationship/relationship.module.ts
+++ standalone/portal-api/src/modules/relationship/relationship.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollaborationRelationshipEntity } from './models/collaboration.relationship.entity';
+import { BuyersModule } from '../buyer/buyers.module';
+import { SuppliersModule } from '../supplier/suppliers.module';
+import { CompaniesModule } from '../company/company.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CollaborationRelationshipEntity])],
+  imports: [
+    CompaniesModule,
+    BuyersModule,
+    SuppliersModule,
+    TypeOrmModule.forFeature([CollaborationRelationshipEntity])
+  ],
   controllers: [],
   providers: [ConfigModule, ConfigService],
 })
