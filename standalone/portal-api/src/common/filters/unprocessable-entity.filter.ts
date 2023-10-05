fe6af03c89872838abaec61900d946dd80895cc4
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-10-05 13:09:47 +0300

    feat(validation): log validation issue

--- standalone/portal-api/src/common/filters/unprocessable-entity.filter.ts
+++ standalone/portal-api/src/common/filters/unprocessable-entity.filter.ts
@@ -1,17 +1,31 @@
-import { ExceptionFilter, Catch, ArgumentsHost, UnprocessableEntityException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, UnprocessableEntityException, Inject, Logger } from '@nestjs/common';
 import { Response } from 'express';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Catch(UnprocessableEntityException)
 export class UnprocessableExceptionFilter implements ExceptionFilter {
-  catch(exception: UnprocessableEntityException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
+    constructor(
+        @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger
+    ) { }
 
-    response.status(status).json({
-      statusCode: status,
-      message: exception.message,
-      errors: exception.getResponse()  // This will contain your error details
-    });
-  }
+    catch(exception: UnprocessableEntityException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const status = exception.getStatus();
+
+        this.logger.error('Error: ', {
+            meta: {
+                name: exception.name,
+                cause: exception.cause,
+                stack: exception.stack,
+                error: exception.message,
+            },
+        });
+
+        response.status(status).json({
+            statusCode: status,
+            message: exception.message,
+            errors: exception.getResponse()  // This will contain your error details
+        });
+    }
 }
