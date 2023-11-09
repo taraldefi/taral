5c17acaeda9de77ecf28283ae58e51d8e6f833e8
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-09 18:40:16 +0200

    chore: continue test conversion

--- packages/clarity/unit-tests/fixtures.ts
+++ packages/clarity/unit-tests/fixtures.ts
@@ -1,9 +1,9 @@
 
 export const parts = [
-    "0x00200020",
-    "0xb9d30838796e6ea7ff4b441ca1d705c229f3492cfdddcd186b21000000000000",
-    "0xed853698ef70b79478b6c01e31efdfff6fac38606661e3aa7b30d1b6fe6bf65a",
-    "0xbec89660",
-    "0x0afd2219",
-    "0x6e2d6012",
+    "00200020",
+    "b9d30838796e6ea7ff4b441ca1d705c229f3492cfdddcd186b21000000000000",
+    "ed853698ef70b79478b6c01e31efdfff6fac38606661e3aa7b30d1b6fe6bf65a",
+    "bec89660",
+    "0afd2219",
+    "6e2d6012",
 ];
