f4454319ebaa9f382510d0810e9c5150a7139723
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 21:02:01 +0200

    feat(marketplace): ensure auction cannot be started if the contract is paused

--- packages/clarity/unit-tests/nft-marketplace-auctions.test.ts
+++ packages/clarity/unit-tests/nft-marketplace-auctions.test.ts
@@ -477,5 +477,63 @@ describe("test marketplace auction flows", () => {
         );
 
         expect(cancelAuctionResult.result).toBeOk(Cl.bool(true));
+    }),
+
+    it("Ensure auction cannot be started if the contract is paused", () => {
+        const setWhitelistedResult = simnet.callPublicFn(
+            "nft-marketplace",
+            "set-whitelisted",
+            [
+                Cl.contractPrincipal(
+                    "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM", "sip009-nft"
+                ),
+                Cl.bool(true),
+            ],
+            DEPLOYER
+        );
+
+        expect(setWhitelistedResult.result).toBeOk(Cl.bool(true));
+
+        let mint = simnet.callPublicFn(
+            "sip009-nft",
+            "mint",
+            [Cl.standardPrincipal(WALLET_1)],
+            DEPLOYER
+        );
+
+        expect(mint.result).toBeOk(Cl.uint(1));
+
+        const nftId = simnet.callReadOnlyFn("sip009-nft", "get-last-token-id", [], DEPLOYER);
+
+        expect(nftId.result).toBeOk(Cl.uint(1));
+
+        const pauseContractResult = simnet.callPublicFn(
+            "nft-marketplace",
+            "pause-contract",
+            [],
+            DEPLOYER
+        );
+
+        expect(pauseContractResult.result).toBeOk(Cl.bool(true));
+
+        const startAuctionResult = simnet.callPublicFn(
+            "nft-marketplace",
+            "start-auction",
+            [
+                Cl.contractPrincipal(
+                    "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM", "sip009-nft"
+                ),
+                Cl.tuple({
+                    "token-id": Cl.uint(1),
+                    "start-block": Cl.uint(100),
+                    "end-block": Cl.uint(1000),
+                    "start-bid": Cl.uint(100),
+                    "reserve-price": Cl.uint(1000),
+                }),
+            ],
+            WALLET_1
+        );
+
+        expect(startAuctionResult.result).toBeErr(Cl.uint(9000));
     })
 });
