3b361fb1d20070ae97891b0ecd6a71c0cfed7858
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 15:15:33 +0200

    feat(exporter): ensure exporter exists after registration

--- packages/clarity/unit-tests/exporter.test.ts
+++ packages/clarity/unit-tests/exporter.test.ts
@@ -91,5 +91,51 @@ describe("test exporter flows", () => {
 
         // ERR-GENERIC
         expect(registerExporterResult.result).toBeOk(Cl.bool(true));
+    }),
+
+    it("Ensure that exporter exists after registration", () => {
+        let exporter_wallet = "ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5";
+        let exporter_name = "ALPS Logistics";
+        let exporter_category = "Merchant";
+
+        const registerExporterResult = simnet.callPublicFn(
+            "taral-exporter",
+            "register",
+            [
+                Cl.standardPrincipal(exporter_wallet),
+                Cl.stringUtf8(exporter_name),
+                Cl.stringUtf8(exporter_category),
+            ],
+            DEPLOYER
+        );
+
+        if (VERBOSE) {
+            console.log("Register Exporter Result:", JSON.stringify(registerExporterResult, null, 2));
+        }
+
+        // ERR-GENERIC
+        expect(registerExporterResult.result).toBeOk(Cl.bool(true));
+        
+        const getExporterResult = simnet.callReadOnlyFn(
+            "taral-exporter",
+            "get-exporter-profile",
+            [
+                Cl.standardPrincipal(exporter_wallet),
+            ],
+            WALLET_1
+        );
+
+        if (VERBOSE) {
+            console.log("Get Exporter Result:", JSON.stringify(getExporterResult.result, null, 2));
+        }
+
+        const expected = Cl.some(Cl.tuple({
+            name: Cl.stringUtf8(exporter_name),
+            category: Cl.stringUtf8(exporter_category),
+            ordersNextAvailId: Cl.uint(0)
+        }));
+
+        expect(getExporterResult.result).toStrictEqual(expected);
+        
     })
 });
