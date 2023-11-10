b6251c059a813c431e02832a6075a90c44240b7c
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 16:37:14 +0200

    feat(importer): ensure that the importer can be registered only once

--- packages/clarity/unit-tests/importer.test.ts
+++ packages/clarity/unit-tests/importer.test.ts
@@ -30,5 +30,41 @@ describe("test importer flows", () => {
 
         // ERR-GENERIC
         expect(registerImporterResult.result).toBeErr(Cl.uint(100));
+    }),
+
+    it("Ensure that importer can register only once with unique wallet id", () => {
+        let importer_wallet = "ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5";
+        let importer_name = "ALPS Logistics";
+        let importer_category = "Merchant";
+
+        let registerImporterResult = simnet.callPublicFn(
+            "taral-exporter",
+            "register",
+            [
+                Cl.standardPrincipal(importer_wallet),
+                Cl.stringUtf8(importer_name),
+                Cl.stringUtf8(importer_category),
+            ],
+            DEPLOYER
+        );
+
+        registerImporterResult = simnet.callPublicFn(
+            "taral-exporter",
+            "register",
+            [
+                Cl.standardPrincipal(importer_wallet),
+                Cl.stringUtf8(importer_name),
+                Cl.stringUtf8(importer_category),
+            ],
+            DEPLOYER
+        );
+
+
+        if (VERBOSE) {
+            console.log("Register Importer Result:", JSON.stringify(registerImporterResult, null, 2));
+        }
+
+        // ERR-IMPORTER-ALREADY-REGISTERED
+        expect(registerImporterResult.result).toBeErr(Cl.uint(121));
     })
 });
