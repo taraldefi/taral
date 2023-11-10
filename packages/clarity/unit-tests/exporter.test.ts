64f1f84b9c9e3818122d854669fb3c6b9ee7b113
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 14:24:29 +0200

    feat(exporter): ensure exporter registration is a success

--- packages/clarity/unit-tests/exporter.test.ts
+++ packages/clarity/unit-tests/exporter.test.ts
@@ -67,5 +67,29 @@ describe("test exporter flows", () => {
 
         // ERR-EXPORTER-ALREADY-REGISTERED
         expect(registerExporterResult.result).toBeErr(Cl.uint(121));
+    }),
+
+    it("Ensure that exporter registration is a success", () => {
+        let exporter_wallet = "ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5";
+        let exporter_name = "ALPS Logistics";
+        let exporter_category = "Merchant";
+
+        const registerExporterResult = simnet.callPublicFn(
+            "taral-exporter",
+            "register",
+            [
+                Cl.standardPrincipal(exporter_wallet),
+                Cl.stringUtf8(exporter_name),
+                Cl.stringUtf8(exporter_category),
+            ],
+            DEPLOYER
+        );
+
+        if (VERBOSE) {
+            console.log("Register Exporter Result:", JSON.stringify(registerExporterResult, null, 2));
+        }
+
+        // ERR-GENERIC
+        expect(registerExporterResult.result).toBeOk(Cl.bool(true));
     })
 });
