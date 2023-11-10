f026087f9a1a5c3c5a232b14aa1cae2d1639c004
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 17:35:47 +0200

    feat(insurance-pool): ensure that valid txs report the correct amount

--- packages/clarity/unit-tests/insurance-pool-audit.test.ts
+++ packages/clarity/unit-tests/insurance-pool-audit.test.ts
@@ -63,5 +63,64 @@ describe("test insurance audit flows", () => {
         }
 
         expect(submitRewardTx.result).toBeErr(Cl.uint(1000));
+    }),
+
+    it("Ensure that valid txs report the correct amount", () => {
+        // const parts = [
+        //     "0x00200020",
+        //     "0xb9d30838796e6ea7ff4b441ca1d705c229f3492cfdddcd186b21000000000000",
+        //     "0xed853698ef70b79478b6c01e31efdfff6fac38606661e3aa7b30d1b6fe6bf65a",
+        //     "0xbec89660",
+        //     "0x0afd2219",
+        //     "0x6e2d6012",
+        //   ];
+
+        const validTransactionModel = Cl.tuple({
+            version: Cl.bufferFromHex("01000000"),
+            ins: Cl.list([
+                Cl.tuple({
+                    outpoint: Cl.tuple({
+                        hash: Cl.bufferFromHex("c8bd3502a21f810da7692e323cc46e0e9ec1def7a93cc610f6d65b60193174e2"),
+                        index: Cl.bufferFromHex("03000000"),
+                    }),
+                    scriptSig:
+                        Cl.bufferFromHex("47304402204ffe267e6b5aab28350be80c1f4ea94424c483f3f44f175594bb6273000f80e8022042ebd5668420c8b29d2ec2791e2c8aa0d7784d8a6283f958fe581e0be129c61b0121037435c194e9b01b3d7f7a2802d6684a3af68d05bbf4ec8f17021980d777691f1d"),
+                    sequence: Cl.bufferFromHex("fdffffff"),
+                }),
+            ]),
+            outs: Cl.list([
+                Cl.tuple({
+                    scriptPubKey:
+                        Cl.bufferFromHex("6a4c5058365b13588072c8b4eca88a505db5c453123c5c91db98d90ac1cd124402dba596531ebf945361dbdbcb0a43e8d6984ab8eee14982d0341eab198fc74d2d917c6d95dc001e21c20008001e1fc2001d02"),
+                    value: Cl.bufferFromHex("0000000000000000"),
+                }),
+                Cl.tuple({
+                    scriptPubKey: Cl.bufferFromHex("76a9146d78de7b0625dfbfc16c3a8a5735f6dc3dc3f2ce88ac"),
+                    value: Cl.bufferFromHex("1027000000000000"),
+                }),
+                Cl.tuple({
+                    scriptPubKey: Cl.bufferFromHex("76a9146c575e9f31715b180b22738136895876ade678cb88ac"),
+                    value: Cl.bufferFromHex("1027000000000000"),
+                }),
+                Cl.tuple({
+                    scriptPubKey: Cl.bufferFromHex("76a914ba27f99e007c7f605a8305e318c1abde3cd220ac88ac"),
+                    value: Cl.bufferFromHex("752f7c5c00000000"),
+                }),
+            ]),
+            locktime: Cl.bufferFromHex("00000000"),
+        });
+
+        const value = simnet.callReadOnlyFn(
+            "insurance-pool-audit",
+            "get-tx-value-for-pool",
+            [validTransactionModel],
+            WALLET_1
+        );
+
+        if (VERBOSE) {
+            console.log("Get Tx Value For Pool Result:", JSON.stringify(value, null, 2));
+        }
+
+        expect(value.result).toBeOk(Cl.uint(10000));
     })
 });
