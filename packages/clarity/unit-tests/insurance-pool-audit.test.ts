22f4825296cd6b8d4d53cc1966305a7743352fe4
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 17:44:25 +0200

    feat(insurance): ensure that valid txs can be reported

--- packages/clarity/unit-tests/insurance-pool-audit.test.ts
+++ packages/clarity/unit-tests/insurance-pool-audit.test.ts
@@ -1,5 +1,6 @@
 import { Cl } from "@stacks/transactions";
 import { describe, expect, it } from "vitest";
+import { parts } from "./fixtures";
 
 const accounts = simnet.getAccounts();
 const WALLET_1 = accounts.get("wallet_1")!;
@@ -122,5 +123,78 @@ describe("test insurance audit flows", () => {
         }
 
         expect(value.result).toBeOk(Cl.uint(10000));
+    }),
+
+    it("Ensure that valid txs can be reported", () => {
+
+        const validTransactionModel = Cl.tuple({
+            version: Cl.bufferFromHex("01000000"),
+            ins: Cl.list([
+                Cl.tuple({
+                    outpoint: Cl.tuple({
+                        hash: Cl.bufferFromHex("c8bd3502a21f810da7692e323cc46e0e9ec1def7a93cc610f6d65b60193174e2"),
+                        index: Cl.bufferFromHex("03000000"),
+                    }),
+                    scriptSig:
+                        Cl.bufferFromHex("47304402204ffe267e6b5aab28350be80c1f4ea94424c483f3f44f175594bb6273000f80e8022042ebd5668420c8b29d2ec2791e2c8aa0d7784d8a6283f958fe581e0be129c61b0121037435c194e9b01b3d7f7a2802d6684a3af68d05bbf4ec8f17021980d777691f1d"),
+                    sequence: Cl.bufferFromHex("fdffffff"),
+                }),
+            ]),
+            outs: Cl.list([
+                Cl.tuple({
+                    scriptPubKey:
+                        Cl.bufferFromHex("6a4c5058365b13588072c8b4eca88a505db5c453123c5c91db98d90ac1cd124402dba596531ebf945361dbdbcb0a43e8d6984ab8eee14982d0341eab198fc74d2d917c6d95dc001e21c20008001e1fc2001d02"),
+                    value: Cl.bufferFromHex("0000000000000000"),
+                }),
+                Cl.tuple({
+                    scriptPubKey: Cl.bufferFromHex("76a9146d78de7b0625dfbfc16c3a8a5735f6dc3dc3f2ce88ac"),
+                    value: Cl.bufferFromHex("1027000000000000"),
+                }),
+                Cl.tuple({
+                    scriptPubKey: Cl.bufferFromHex("76a9146c575e9f31715b180b22738136895876ade678cb88ac"),
+                    value: Cl.bufferFromHex("1027000000000000"),
+                }),
+                Cl.tuple({
+                    scriptPubKey: Cl.bufferFromHex("76a914ba27f99e007c7f605a8305e318c1abde3cd220ac88ac"),
+                    value: Cl.bufferFromHex("752f7c5c00000000"),
+                }),
+            ]),
+            locktime: Cl.bufferFromHex("00000000"),
+        });
+
+        const result = simnet.callPublicFn(
+            "insurance-pool-audit",
+            "submit-reward-tx",
+            [
+                // block
+                Cl.tuple({
+                    version: Cl.bufferFromHex(parts[0]),
+                    parent: Cl.bufferFromHex(parts[1]),
+                    "merkle-root": Cl.bufferFromHex(parts[2]),
+                    timestamp: Cl.bufferFromHex(parts[3]),
+                    nbits: Cl.bufferFromHex(parts[4]),
+                    nonce: Cl.bufferFromHex(parts[5]),
+                    height: Cl.uint(11319),
+                }),
+                // tx
+                validTransactionModel,
+
+                // proof
+                Cl.tuple({
+                    "tx-index": Cl.uint(6),
+                    hashes: Cl.list([
+                        Cl.bufferFromHex("3ae3dfeedc6eb99fb5e2c5d0c90697a66de969c3f4d974ebe2ef104fcea7f13b"),
+                        Cl.bufferFromHex("52500d11cabf1049ebb139a82b439d08bd3a8e867a41fb3f368dfa125e043989"),
+                        Cl.bufferFromHex("a104c2725aabf28fcf3c304fd370610370330c546495acd5015ecc177c6494f6"),
+                        Cl.bufferFromHex("5e4442a235be2fc92aa15ba3b59c5af61c46dff8e7ed8198ebc48ec6d71a6a49"),
+                        Cl.bufferFromHex("904640bdf50c8edd12232efc41966a3a9af955208b205a90fc8a6dca5f69c458"),
+                    ]),
+                    "tree-depth": Cl.uint(5),
+                }),
+            ],
+            WALLET_1
+        );
+
+        expect(result.result).toBeErr(Cl.uint(1000));
     })
 });
