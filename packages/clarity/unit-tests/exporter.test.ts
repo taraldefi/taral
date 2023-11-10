006478819b5ca2a9657423513599b2cc57f7b3d7
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 14:23:03 +0200

    feat(exporter): exporter can be registered only once

--- packages/clarity/unit-tests/exporter.test.ts
+++ packages/clarity/unit-tests/exporter.test.ts
@@ -14,7 +14,6 @@ describe("test exporter flows", () => {
         let exporter_name = "ALPS Logistics";
         let exporter_category = "";
 
-
         const registerExporterResult = simnet.callPublicFn(
             "taral-exporter",
             "register",
@@ -26,7 +25,47 @@ describe("test exporter flows", () => {
             DEPLOYER
         );
 
-        console.log("Register Exporter Result:", JSON.stringify(registerExporterResult, null, 2));
+        if (VERBOSE) {
+            console.log("Register Exporter Result:", JSON.stringify(registerExporterResult, null, 2));
+        }
+
+        // ERR-GENERIC
         expect(registerExporterResult.result).toBeErr(Cl.uint(100));
+    }),
+
+    it("Ensure that exporter can register only once with unique wallet id", () => {
+        let exporter_wallet = "ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5";
+        let exporter_name = "ALPS Logistics";
+        let exporter_category = "Merchant";
+
+        let registerExporterResult = simnet.callPublicFn(
+            "taral-exporter",
+            "register",
+            [
+                Cl.standardPrincipal(exporter_wallet),
+                Cl.stringUtf8(exporter_name),
+                Cl.stringUtf8(exporter_category),
+            ],
+            DEPLOYER
+        );
+
+        registerExporterResult = simnet.callPublicFn(
+            "taral-exporter",
+            "register",
+            [
+                Cl.standardPrincipal(exporter_wallet),
+                Cl.stringUtf8(exporter_name),
+                Cl.stringUtf8(exporter_category),
+            ],
+            DEPLOYER
+        );
+        
+
+        if (VERBOSE) {
+            console.log("Register Exporter Result:", JSON.stringify(registerExporterResult, null, 2));
+        }
+
+        // ERR-EXPORTER-ALREADY-REGISTERED
+        expect(registerExporterResult.result).toBeErr(Cl.uint(121));
     })
 });
