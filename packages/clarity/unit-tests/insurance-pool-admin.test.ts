de4b2595b8e31713d321ec58c09d82ffdd4cce93
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 17:16:02 +0200

    feat(insurance-pool): sanity check

---
+++ packages/clarity/unit-tests/insurance-pool-admin.test.ts
@@ -0,0 +1,22 @@
+import { Cl } from "@stacks/transactions";
+import { describe, expect, it } from "vitest";
+
+const accounts = simnet.getAccounts();
+const WALLET_1 = accounts.get("wallet_1")!;
+
+describe("test insurance pool", () => {
+    it("Blank", () => {
+        expect(true).toBeTruthy();
+    }),
+
+    it("Sanity check", () => {
+        const payInResult = simnet.callPublicFn(
+            "insurance-pool-admin",
+            "payin",
+            [Cl.uint(1000000), Cl.uint(1)],
+            WALLET_1
+        );
+
+        expect(payInResult.result).toBeOk(Cl.bool(true));
+    })
+});
