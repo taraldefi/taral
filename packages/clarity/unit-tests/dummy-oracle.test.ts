bb221f02b5a5a099fda43c8a97bb6cdf9d2ae192
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-11-10 12:42:28 +0200

    feat(tests): port dummy oracle tests

---
+++ packages/clarity/unit-tests/dummy-oracle.test.ts
@@ -0,0 +1,37 @@
+import { Cl } from "@stacks/transactions";
+import { describe, expect, it } from "vitest";
+
+const accounts = simnet.getAccounts();
+const WALLET_1 = accounts.get("wallet_1")!;
+
+const VERBOSE = false;
+
+describe("test dummy oracle", () => {
+    it("Ensure that the prices are fetched", () => {
+        const btcPriceResult = simnet.callReadOnlyFn(
+            "dummy-oracle",
+            "get-btc-price",
+            [],
+            WALLET_1
+        );
+
+        if (VERBOSE) {
+            console.log("BTC Price Result:", JSON.stringify(btcPriceResult, null, 2));
+        }
+
+        expect(btcPriceResult.result).toBeOk(Cl.uint(50000));
+
+        const stxPriceResult = simnet.callReadOnlyFn(
+            "dummy-oracle",
+            "get-stx-price",
+            [],
+            WALLET_1
+        );
+
+        if (VERBOSE) {
+            console.log("STX Price Result:", JSON.stringify(stxPriceResult, null, 2));
+        }
+
+        expect(stxPriceResult.result).toBeOk(Cl.uint(2));
+    })
+});
