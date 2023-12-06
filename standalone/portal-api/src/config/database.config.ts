19c55e4ca42ab49c6128677eff0691540104e0fa
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-12-06 14:50:32 +0200

    chore(config): cleanup

--- standalone/portal-api/src/config/database.config.ts
+++ standalone/portal-api/src/config/database.config.ts
@@ -9,7 +9,7 @@ export default registerAs('database', () => ({
   password: process.env.DATABASE_PASSWORD || config.get('db.password'),
   name: process.env.DATABASE_NAME || config.get('db.name'),
   username: process.env.DATABASE_USERNAME || config.get('db.username'),
-  synchronize: (process.env.DATABASE_SYNCHRONIZE || config.get('db.syncronize')) === 'true',
+  synchronize: (process.env.DATABASE_SYNCHRONIZE || config.get('db.synchronize')) === 'true',
   maxConnections: parseInt(process.env.DATABASE_MAX_CONNECTIONS || config.get('db.maxConnections'), 10) || 100,
   sslEnabled: (process.env.DATABASE_SSL_ENABLED || config.get('db.sslEnabled')) === 'true',
   rejectUnauthorized: (process.env.DATABASE_REJECT_UNAUTHORIZED || config.get('db.rejectUnauthorized')) === 'true',
