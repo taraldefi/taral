1e416526938a807408d876196083f627ef0859d0
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2023-12-07 11:57:56 +0200

    feat(bank): test to ensure that purchase order creation and cancellation happens correctly

--- packages/clarity/unit-tests/taral-bank.test.ts
+++ packages/clarity/unit-tests/taral-bank.test.ts
@@ -1,4 +1,8 @@
+import { initSimnet } from "@hirosystems/clarinet-sdk";
+import { Cl } from "@stacks/transactions";
 import { describe, expect, it } from "vitest";
+
+const simnet = await initSimnet();
 
 const accounts = simnet.getAccounts();
 const WALLET_1 = accounts.get("wallet_1")!;
@@ -7,6 +11,127 @@ const DEPLOYER = accounts.get("deployer")!;
 
 describe("Taral bank test flows", () => {
   it("Should be able to create a purchase order", () => {
-    expect(true).toBeTruthy();
+    const borrow = 1100;
+    const downPayment = 100;
+    const purchaseOrderId = 1;
+
+    const purchaseOrderResult = simnet.callPublicFn(
+        "taral-bank",
+        "create-purchase-order",
+        [
+            Cl.uint(borrow),
+            Cl.uint(downPayment),
+            Cl.standardPrincipal(WALLET_2)
+        ], WALLET_1
+    );
+
+    expect(purchaseOrderResult.result).toBeOk(Cl.uint(purchaseOrderId));
+
+    expectUsdaTransfer(purchaseOrderResult.events[0].data, WALLET_1, DEPLOYER, downPayment);
+
+    const getPurchaseOrder = simnet.callReadOnlyFn(
+        "taral-bank",
+        "get-purchase-order-by-id",
+        [Cl.uint(purchaseOrderId)],
+        WALLET_1,
+    );
+
+    expect(getPurchaseOrder.result).toStrictEqual(Cl.some(Cl.tuple({
+        "borrower-id": Cl.standardPrincipal(WALLET_1),
+        "completed-successfully": Cl.bool(false),
+        "created-at": Cl.uint(4),
+        "first-payment-month": Cl.uint(0),
+        "first-payment-year": Cl.uint(0),
+        "seller-id": Cl.standardPrincipal(WALLET_2),
+        "accepted-financing-id": Cl.none(),
+        "total-amount": Cl.uint(borrow),
+        "payments-left": Cl.uint(0),
+        "lender-id": Cl.none(),
+        "is-canceled": Cl.bool(false),
+        "is-completed": Cl.bool(false),
+        "has-active-financing": Cl.bool(false),
+        "outstanding-amount": Cl.uint(borrow - downPayment),
+        "overpaid-balance": Cl.uint(0),
+        "updated-at": Cl.uint(4),
+        downpayment: Cl.uint(downPayment),
+    })));
+
+    // check if the PO has active financing
+
+    const checkHasActiveFinancingResult = simnet.callReadOnlyFn(
+        "taral-bank",
+        "has-active-financing",
+        [
+            Cl.uint(purchaseOrderId),
+        ], 
+        WALLET_1
+    );
+
+    expect(checkHasActiveFinancingResult.result).toStrictEqual(Cl.bool(false));
+
+    const cancelPurchaseOrderResult = simnet.callPublicFn(
+        "taral-bank",
+        "cancel-purchase-order",
+        [
+            Cl.uint(purchaseOrderId),
+        ], 
+        WALLET_1
+    );
+
+    expect(cancelPurchaseOrderResult.result).toBeOk(Cl.bool(true));
+
+    expectUsdaTransfer(cancelPurchaseOrderResult.events[0].data, DEPLOYER, WALLET_1, downPayment);
+
+    const getPurchaseOrderAfterCancel = simnet.callReadOnlyFn(
+        "taral-bank",
+        "get-purchase-order-by-id",
+        [Cl.uint(purchaseOrderId)],
+        WALLET_1,
+    );
+
+    expect(getPurchaseOrderAfterCancel.result).toStrictEqual(Cl.some(Cl.tuple({
+        "borrower-id": Cl.standardPrincipal(WALLET_1),
+        "completed-successfully": Cl.bool(false),
+        "created-at": Cl.uint(4),
+        "first-payment-month": Cl.uint(0),
+        "first-payment-year": Cl.uint(0),
+        "seller-id": Cl.standardPrincipal(WALLET_2),
+        "accepted-financing-id": Cl.none(),
+        "total-amount": Cl.uint(borrow),
+        "payments-left": Cl.uint(0),
+        "lender-id": Cl.none(),
+        "is-canceled": Cl.bool(true),
+        "is-completed": Cl.bool(false),
+        "has-active-financing": Cl.bool(false),
+        "outstanding-amount": Cl.uint(borrow - downPayment),
+        "overpaid-balance": Cl.uint(0),
+        "updated-at": Cl.uint(4),
+        downpayment: Cl.uint(downPayment),
+    })));
   })
+
+  /*
+   * Helper function to assert that a transfer event is a USDa transfer 
+  */
+  function expectUsdaTransfer(transferEvent: any, sender: string, recipient: string, amount: any) {
+
+    let senderAddress = sender;
+    if (sender === DEPLOYER) {
+        senderAddress = `${sender}.taral-bank`;
+    }
+
+    let recipientAddress = recipient;
+    if (recipient === DEPLOYER) {
+        recipientAddress = `${recipient}.taral-bank`;
+    }
+
+
+    expect(transferEvent.asset_identifier).toStrictEqual(
+        `${DEPLOYER}.usda-token::usda`,
+    );
+
+    expect(transferEvent.sender).toStrictEqual(senderAddress);
+    expect(transferEvent.recipient).toStrictEqual(recipientAddress);
+    expect(transferEvent.amount).toStrictEqual(`${amount}`);
+  }
 });
