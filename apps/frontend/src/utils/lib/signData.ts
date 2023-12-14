23614b0136316ed838e35bd3c3be863ffe192849
Zishan <amanzishan.az@gmail.com>
2023-12-14 22:01:05 +0530

    feat(sign-data): signing transaction

--- apps/frontend/src/utils/lib/signData.ts
+++ apps/frontend/src/utils/lib/signData.ts
@@ -1,27 +1,16 @@
-import { StacksNetwork } from "@stacks/network";
-import { openSignatureRequestPopup } from "@stacks/connect";
+import { StacksNetwork } from "micro-stacks/network";
+import { useOpenSignMessage } from "@micro-stacks/react";
 
 export function signData(
   message: string,
   network: StacksNetwork
 ): Promise<any> {
+  const { openSignMessage, isRequestPending } = useOpenSignMessage();
   return new Promise((resolve, reject) => {
-    openSignatureRequestPopup({
+    openSignMessage({
       message,
-      network: network, // for mainnet, `new StacksMainnet()`
-      appDetails: {
-        name: "Tariala",
-        icon: window.location.origin + "/my-app-logo.svg",
-      },
-      onFinish(data) {
-        if (data) {
-          resolve(data); // Resolve the promise with the data
-        } else {
-          reject("No data"); // Reject the promise if there's no data
-        }
-      },
-      onCancel() {
-        reject("Signature request cancelled"); // Reject the promise on cancel
+      onFinish: (walletResponse) => {
+        console.log("walletResponse", walletResponse);
       },
     });
   });
