f119b170e878855544635b8f2c8454a728346af5
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2024-02-29 11:01:21 +0200

    chore(frontend): copy the public folder next to .next

--- apps/frontend/copy-assets.js
+++ apps/frontend/copy-assets.js
@@ -18,11 +18,11 @@ fs.copy('./public', './.next/standalone/apps/frontend/.next/public', function (e
     }
 });
 
-// fs.copy('./public', './.next/standalone/apps/frontend/.next/public/images', function (err) {
-//     if (err) {
-//       console.error('Error occurred while copying the public folder.', err);
-//     } else {
-//       console.log('Public directory copied to standalone!');
-//     }
-// });
+fs.copy('./public', './.next/standalone/apps/frontend/public', function (err) {
+    if (err) {
+      console.error('Error occurred while copying the public folder.', err);
+    } else {
+      console.log('Public directory copied to standalone!');
+    }
+});
     
