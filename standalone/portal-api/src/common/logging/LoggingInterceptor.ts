78105dc7ed02856625ac433b11771d18454cb9c7
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2024-03-21 12:52:35 +0200

    feat(logging): do not log the response as it might get very large

--- standalone/portal-api/src/common/logging/LoggingInterceptor.ts
+++ standalone/portal-api/src/common/logging/LoggingInterceptor.ts
@@ -1,73 +1,79 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
-    CallHandler,
-    ExecutionContext,
-    Injectable,
-    NestInterceptor,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
 } from "@nestjs/common";
-import {tap} from "rxjs";
 import CoreLoggerService from "./CoreLoggerService";
 import { Reflector } from "@nestjs/core";
 import { PARSE_REQUEST_BODY_WHEN_LOGGING_KEY } from "./parseRequestBodyWhenLogging";
 import { plainToInstance } from "class-transformer";
+import { tap } from "rxjs";
 
-export type Constructor<T, Arguments extends unknown[] = any[]> = new(...arguments_: Arguments) => T;
+const SHOULD_LOG_RESPONSE: boolean = false;
+
+export type Constructor<T, Arguments extends unknown[] = any[]> = new (...arguments_: Arguments) => T;
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-    constructor(private readonly logger: CoreLoggerService, private readonly reflector: Reflector) {}
+  constructor(private readonly logger: CoreLoggerService, private readonly reflector: Reflector) { }
 
-    private parseRequestBody(context: ExecutionContext) {
-        const httpContext = context.switchToHttp();
-        const request = httpContext.getRequest<Request>();
-        const body: unknown = request.body;
-        if (!body) {
-          return;
-        }​
-        
-        if (body === null || typeof body !== 'object') {
-          return body;
-        }
-        const requestBodyDto = this.reflector.getAllAndOverride<
-          Constructor<unknown>
-        >(PARSE_REQUEST_BODY_WHEN_LOGGING_KEY, [
-          context.getHandler(),
-          context.getClass(),
-        ]);
-        if (!requestBodyDto) {
-          return JSON.stringify(body);
-        }
-        const instance = plainToInstance(requestBodyDto, body);
-        return JSON.stringify(instance, null, 2);
-      }
+  private parseRequestBody(context: ExecutionContext) {
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest<Request>();
+    const body: unknown = request.body;
+    if (!body) {
+      return;
+    }
 
-    intercept(context: ExecutionContext, next: CallHandler) {
-        const request = context.switchToHttp().getRequest();
+    if (body === null || typeof body !== 'object') {
+      return body;
+    }
+    const requestBodyDto = this.reflector.getAllAndOverride<
+      Constructor<unknown>
+    >(PARSE_REQUEST_BODY_WHEN_LOGGING_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requestBodyDto) {
+      return JSON.stringify(body);
+    }
+    const instance = plainToInstance(requestBodyDto, body);
+    return JSON.stringify(instance, null, 2);
+  }
 
-        const response = context.switchToHttp().getResponse();
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest();
 
-        const {originalUrl, method, params, query } = request;
-        const message = `${method} ${originalUrl} 
+    const response = context.switchToHttp().getResponse();
+
+    const { originalUrl, method, params, query } = request;
+    const message = `${method} ${originalUrl} 
         Request params: ${JSON.stringify(params, null, 2)}
         Request query: ${JSON.stringify(query, null, 2)}
         Request body: ${this.parseRequestBody(context)}`;
 
-        this.logger.log(message);
+    if (!SHOULD_LOG_RESPONSE) {
+      this.logger.log(message);
+    } else {
+      return next.handle().pipe(
+        tap((data) => {
+          const message = `type: RESPONSE, ${method} ${originalUrl} Response body: ${JSON.stringify(data, null, 2)}`;
 
-        return next.handle().pipe(
-            tap((data) => {
-                const message = `type: RESPONSE, ${method} ${originalUrl} Response body: ${JSON.stringify(data, null, 2)}`;
+          if (response?.status >= 500) {
+            this.logger.error(message);
+          }
 
-                if (response?.status >= 500) {
-                    this.logger.error(message);
-                }
+          if (response?.status >= 400) {
+            this.logger.warn(message);
+          }
 
-                if (response?.status >= 400) {
-                    this.logger.warn(message);
-                }
-
-                this.logger.log(message);
-            })
-        );
+          this.logger.log(message);
+        })
+      );
     }
+
+    return next.handle();
+  }
 }
