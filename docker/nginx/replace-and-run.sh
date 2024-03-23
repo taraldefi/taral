fcf8df46b9a192352dd1e34623b4bbf786f709e5
Cioclea Doru Octavian <doru.cioclea@corebuild.eu>
2024-03-23 13:43:23 +0200

    chore(nginx): dockerfile and template environment substitution

---
+++ docker/nginx/replace-and-run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Use envsubst to substitute the environment variables
+envsubst '$API_DOMAIN' < /etc/nginx/sites-enabled/backend.template > /etc/nginx/sites-enabled/backend.conf
+envsubst '$FRONTEND_DOMAIN' < /etc/nginx/sites-enabled/frontend.template > /etc/nginx/sites-enabled/frontend.conf
+envsubst '$SEQ_DOMAIN' < /etc/nginx/sites-enabled/seq.template > /etc/nginx/sites-enabled/seq.conf
+envsubst '$SQLPAD_DOMAIN' < /etc/nginx/sites-enabled/sqlpad.template > /etc/nginx/sites-enabled/sqlpad.conf
+
+# Start Nginx in the foreground
+nginx -g 'daemon off;'
